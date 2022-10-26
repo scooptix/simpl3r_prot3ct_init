bc444cb10715dced219b9466821d2b0f824b895d
Alex-ScoopTix <alex@scooptix.com>
2022-10-26 13:31:45 +0800

    Added batches to execution

--- utils/theft_protection.ts
+++ utils/theft_protection.ts
@@ -19,35 +19,44 @@ export async function updateMetadataUrls({
     .use(keypairIdentity(Keypair.fromSecretKey(UAPrivateKey)))
     .use(bundlrStorage());
   let i = 0;
+  const total = mintIds.length;
   try {
     while (mintIds.length > 0) {
-      const batch = mintIds.splice(0, 100);
+      // Execute 100 at a time, waiting for the slowest request to finish before starting the next batch
+      // That should create enough delay to not hit the RPC Node's rate limits
+      const BATCH_SIZE = 100; // Change this if your RPC node has a rate limit of less than 100 requests per second
+      const batch = mintIds.splice(0, BATCH_SIZE);
 
       await Promise.all(
         batch.map(async (mintId) => {
-          const nft = await metaplex
-            .nfts()
-            .findByMint({ mintAddress: toPublicKey(mintId) })
-            .run();
+          try {
+            const nft = await metaplex
+              .nfts()
+              .findByMint({ mintAddress: toPublicKey(mintId) })
+              .run();
 
-          //nfts().update() sometimes changes the data but times out, if that happens make sure we continue
-          // https://github.com/metaplex-foundation/js/issues/331
-          setTimeout(() => {
-            return;
-          }, 30000);
+            //nfts().update() sometimes changes the data but times out, if that happens make sure we continue
+            // https://github.com/metaplex-foundation/js/issues/331
+            setTimeout(() => {
+              return;
+            }, 30000);
 
-          // await metaplex
-          //   .nfts()
-          //   .update({
-          //     nftOrSft: nft,
-          //     uri: `https://us-central1-simpl3r.cloudfunctions.net/m?id=${mintId}&pid=${projectId}`,
-          //   })
-          //   .run();
-
+            await metaplex
+              .nfts()
+              .update({
+                nftOrSft: nft,
+                uri: `https://us-central1-simpl3r.cloudfunctions.net/m?id=${mintId}&pid=${projectId}`,
+              })
+              .run();
+          } catch (_) {
+            console.log(
+              `An error occurred updating the NFT with mint ID ${mintId}. The update most likely didn't complete. You can use the file "update_with_mintIds" to rerun any failed updates. If this occurres frequently, please make sure your RPC node can handle ~100 requests / second or reduce the batch size above`
+            );
+          }
           return;
         })
       );
-      console.log(`Progress: ${++i * 100}/${mintIds.length}`);
+      console.log(`Progress: ${Math.min(++i * BATCH_SIZE, total)}/${total}`);
     }
 
     return true;
